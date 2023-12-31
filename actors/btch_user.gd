92a514ca7140501391e85aa06811800f0fd37aa5
Quim Nuss Székely <quimnuss@altaveu>
2023-12-31 20:15:02 +0100

    spaghetti to get two instances use 2 configs on editor

--- actors/btch_user.gd
+++ actors/btch_user.gd
@@ -5,37 +5,66 @@ class_name BtchUser
 var auth_endpoint : String = "%s/token" % BtchCommon.BASE_URL
 @onready var user_seq_request : HTTPRequest = $UserSeqRequests
 
-@export var username : String = 'foo'
+@export var username : String = 'Steve'
+@export var email : String = 'steve@example.com'
 @export var plain_password : String = 'foo'
 @export var hash_password : String = ''
 
-var config = ConfigFile.new()
+var config : ConfigFile = ConfigFile.new()
 # NOTE on linux user:// is .local/share/godot/app_userdata/battlechess/
-var CONFIG_FILE : String = "user://btch.cfg"
+
+var player_section : String = "Player"
 
 func _ready():
-    var err = config.load(CONFIG_FILE)
+        
+    dev_run_setup()    
+    
+    var err = config.load(Globals.CONFIG_FILE_ACTIVE)
     if err != OK:
-        prints("User config",CONFIG_FILE,"failed to load.")
+        prints("User config",Globals.CONFIG_FILE_ACTIVE,"failed to load.")
+        init_config_file()
 
-        config.set_value("Player", "username", "Steve")
-        config.set_value("Player", "plain_password", "foo")
-        config.save(CONFIG_FILE)
-    else:
-        username = config.get_value("Player", "username", "Steve")
-        # TODO plain_password unnecessary, only hashed will be saved. SignUp page will use ram for plain_password
-        plain_password = config.get_value("Player", "plain_password", "foo")
-        hash_password = config.get_value("Player", "hash_password", "")
+    config.load(Globals.CONFIG_FILE_ACTIVE)
+    username = config.get_value(player_section, "username", username)
+    email = config.get_value(player_section, "email", email)
+    # TODO plain_password unnecessary, only hashed will be saved. SignUp page will use ram for plain_password
+    plain_password = config.get_value(player_section, "plain_password", plain_password)
+    hash_password = config.get_value(player_section, "hash_password", "")
 
-    var ok : int = await create_user(username, plain_password)
+    var ok : int = await create_user(username, email, plain_password)
     if ok != OK and ok != ERR_ALREADY_EXISTS:
         prints("failed remote creating user")
     else:
         prints("user",username,"OK.","exists?",ok==ERR_ALREADY_EXISTS)
-        
 
-func create_user(username, password) -> Error:
-    var credentials : Dictionary = {'username': username, 'plain_password': password}
+
+func init_config_file():
+    if OS.has_feature("debug"):
+        var config_dev1 = ConfigFile.new()
+        config_dev1.set_value("Player", "username", username)
+        config_dev1.set_value("Player", "email", email)
+        config_dev1.set_value("Player", "plain_password", plain_password)
+        config_dev1.save(Globals.CONFIG_FILE_DEV1)
+        var config_dev2 = ConfigFile.new()
+        config_dev2.set_value("PlayerDev", "username", "Maya")
+        config_dev2.set_value("PlayerDev", "email", "maya@example.com")
+        config_dev2.set_value("PlayerDev", "plain_password", "bar")
+        config_dev2.save(Globals.CONFIG_FILE_DEV2)
+
+func dev_run_setup():
+    # Switch the section if we're running two instances from the editor
+    if OS.has_feature("debug"):
+        var process_id : int = OS.get_process_id()
+        if process_id%2 == 1:
+            player_section = "PlayerDev"
+            Globals.CONFIG_FILE_ACTIVE = Globals.CONFIG_FILE_DEV2
+        else:
+            Globals.CONFIG_FILE_ACTIVE = Globals.CONFIG_FILE_DEV1
+        DisplayServer.window_set_title(player_section+'-'+str(process_id))
+        prints("Player config section is", player_section)
+
+func create_user(username, email, password) -> Error:
+    var credentials : Dictionary = {'username': username, 'email': email, 'plain_password': password}
     
     var payload : String = JSON.stringify(credentials)
     
@@ -54,11 +83,13 @@ func create_user(username, password) -> Error:
     print(JSON.stringify(json,'  '))
     
     if response_code == 200:
-        
         var hashed_password : String = json['hashed_password']
+        prints("save hashed password for",username,"on section",player_section,"config",Globals.CONFIG_FILE_ACTIVE,"value",hashed_password)
         
-        config.set_value("Player", "hash_password", hashed_password)
-        config.save(CONFIG_FILE)
+        config.load(Globals.CONFIG_FILE_ACTIVE)
+        config.set_value(player_section, "hash_password", hashed_password)
+        config.save(Globals.CONFIG_FILE_ACTIVE)
+        prints("saved hash",config.get_value(player_section, "hash_password"))
         return OK
     elif response_code == 409:
         prints("user", username, "already taken")
