eb07bbfbf7b7d7a73c0b64d4f3f8f597ac34126b
Quim Nuss Székely <quimnuss@altaveu>
2024-01-01 17:51:32 +0100

    add globals and other multiinstance stuff

---
+++ actors/globals.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var CONFIG_FILE_ACTIVE : String = "user://btch.cfg"
+const CONFIG_FILE : String = "user://btch.cfg"
+const CONFIG_FILE_DEV1 : String = "user://btch_dev1.cfg"
+const CONFIG_FILE_DEV2 : String = "user://btch_dev2.cfg"
