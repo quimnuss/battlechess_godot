1d506d7a36e046613cab90efa03c50e9ff4fe494
Quim Nuss Székely <quimnuss@altaveu>
2024-01-07 12:24:24 +0100

    support window resizing, support api responding array

--- actors/btch_game.gd
+++ actors/btch_game.gd
@@ -3,15 +3,33 @@ extends Node
 
 class_name BtchGame
 
-var game_endpoint : String = "/games"
+const game_endpoint : String = "/games"
+
+const users_games_endpoint : String = "/users/me/games"
 
 @onready var http_request = $HTTPRequest
+
+signal game_joined(uuid : String)
 
 var uuid : String
 
 func join_open_game() -> BtchCommon.HTTPStatus:
-    var status_code : BtchCommon.HTTPStatus = await BtchCommon.btch_standard_request(game_endpoint, {}, http_request, HTTPClient.METHOD_PATCH)
-    return status_code
+    # first try to join one of my games
+    var data : Dictionary = await BtchCommon.btch_standard_data_request(users_games_endpoint, {}, http_request, HTTPClient.METHOD_GET)
+    
+    if data['status_code'] != BtchCommon.HTTPStatus.OK:
+        return data['status_code']
+    
+    if len(data['data']) > 0:
+        # we have games!
+        var games = data['data']
+        game_joined.emit(games[0].uuid)
+        return BtchCommon.HTTPStatus.OK
+    else:
+        var game : Dictionary = await BtchCommon.btch_standard_data_request(game_endpoint, {}, http_request, HTTPClient.METHOD_PATCH)
+        if game['status_code'] == BtchCommon.HTTPStatus.OK:
+            game_joined.emit(game.uuid)
+        return game['status_code']
 
 func create_and_join_game() -> BtchCommon.HTTPStatus:
     var response_data : Dictionary = await BtchCommon.btch_standard_data_request(game_endpoint, {'public' : true}, http_request, HTTPClient.METHOD_POST)
