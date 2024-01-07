1c9c6ef9b725b498497fc4addbcacd63d064d813
Pol Monsó Purtí <pol@altaveu>
2024-01-07 22:36:04 +0100

    fill up Game from json payload

--- actors/btch_game.gd
+++ actors/btch_game.gd
@@ -13,6 +13,15 @@ signal game_joined(uuid : String)
 
 var uuid : String
 
+var game_owner : BtchUser
+var white_player : BtchUser
+var black_player : BtchUser
+var game_status
+var player_turn : BtchUser
+var last_move_time : int
+var is_public_game : bool
+var winner : BtchUser
+
 func join_open_game() -> BtchCommon.HTTPStatus:
     # first try to join one of my games
     var data : Dictionary = await BtchCommon.btch_standard_data_request(users_games_endpoint, {}, http_request, HTTPClient.METHOD_GET)
@@ -24,12 +33,14 @@ func join_open_game() -> BtchCommon.HTTPStatus:
         # we have games!
         var games = data['data']
         uuid = games[0].uuid
+        _from_dict(games[0])
         game_joined.emit(uuid)
         return BtchCommon.HTTPStatus.OK
     else:
         var game : Dictionary = await BtchCommon.btch_standard_data_request(game_endpoint, {}, http_request, HTTPClient.METHOD_PATCH)
         if game['status_code'] == BtchCommon.HTTPStatus.OK:
             uuid = game.uuid
+            _from_dict(game)
             game_joined.emit(uuid)
         return game['status_code']
 
@@ -45,5 +56,19 @@ func join_game(game_uuid : String) -> BtchCommon.HTTPStatus:
     var response_data : Dictionary = await BtchCommon.btch_standard_data_request(game_endpoint + '/' + game_uuid +'/join', {}, http_request, HTTPClient.METHOD_GET)
     if response_data['status_code'] == BtchCommon.HTTPStatus.OK:
         uuid = response_data['uuid']
+        _from_dict(response_data)
         game_joined.emit(uuid)
     return response_data['status_code']
+
+func _from_dict(game_dict : Dictionary):
+    uuid = game_dict['uuid']
+    
+    game_owner = game_dict['owner_id']
+    white_player = game_dict['white_id']
+    black_player = game_dict['black_id']
+    
+    game_status = game_dict['status']
+    player_turn = game_dict['turn']
+    last_move_time = game_dict['last_move_time']
+    is_public_game = game_dict['public']
+    winner = game_dict['winner']
