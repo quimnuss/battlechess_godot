a66d79bafba86918ceb1d950fce9958c3d986be7
Quim Nuss Székely <quimnuss@altaveu>
2024-01-15 22:24:47 +0100

    fix main menu level start, ignore config if not loaded

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -5,7 +5,7 @@ extends Control
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
 @onready var username_text_edit = $MarginContainer/HBoxContainer/VBoxContainer/UsernameLineEdit
 @onready var password_text_edit = $MarginContainer/HBoxContainer/VBoxContainer/PasswordLineEdit
-#@onready var start_level = preload("res://ui/lobby.tscn") as PackedScene
+@onready var start_level = preload("res://ui/lobby.tscn") as PackedScene
 
 var config : ConfigFile = ConfigFile.new()
 
@@ -14,13 +14,13 @@ func _ready():
     var err = config.load(Globals.CONFIG_FILE_ACTIVE)
     if err != OK:
         prints("User config",Globals.CONFIG_FILE_ACTIVE,"failed to load.")
-    
-    var username = config.get_value('Player','username')
-    if username:
-        username_text_edit.text = username
-    var password = config.get_value('Player','password')
-    if password:
-        password_text_edit.text = password
+    else:
+        var username = config.get_value('Player','username')
+        if username:
+            username_text_edit.text = username
+        var password = config.get_value('Player','password')
+        if password:
+            password_text_edit.text = password
         
     start_button.button_down.connect(on_start_pressed)
     exit_button.button_down.connect(on_exit_pressed)
