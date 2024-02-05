de540aebe0a84506ee4ca3241c2b4fb5e387693e
quimnuss <quimnuss@altaveu>
2024-02-05 20:55:21 +0100

    empty commit

--- ui/menu_ingame.gd
+++ ui/menu_ingame.gd
@@ -1,7 +1,8 @@
 extends Control
 @onready var navigation_layer = $NavigationLayer
 
-const MAIN_MENU : String = "res://ui/menu_main.tscn"
+const MAIN_MENU: String = "res://ui/menu_main.tscn"
+
 
 func _on_exit_button_pressed():
     get_tree().quit()
@@ -18,6 +19,6 @@ func _on_navigation_layer_menu_pressed():
 func _on_log_out_button_pressed():
     BtchCommon.username = ""
     BtchCommon.password = ""
-    
+
     prints("going to welcome screen")
     get_tree().change_scene_to_file(MAIN_MENU)
